2736e9da9bc8c82b3e9a84f8528310fceb838744
NAQLETO <naqleto@gmail.com>
2022-12-02 15:38:18 -0300

    estilização

---
+++ src/Pages/start/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, Image, ScrollView, StyleSheet } from "react-native";
+
+
+export default function Start() {
+  return (
+    <View style={style.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{alignItems: "center"}}>
+          <Image
+            source={require("../../assets/icons/logo3.png")}
+            style={style.logo}
+          />
+          <Text style={style.description}>
+            Vamos transformar sua vida {"\n"} em um jogo, buscando sempre {"\n"} o melhor nível.
+          </Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21, 21, 21, 0.98)",
+  },
+
+  logo: {
+    width: 300,
+    height: 60,
+    marginTop: 60,
+    marginBottom: 20,
+  },
+  description: {
+    color: "#ffffff",
+    fontSize: 20,
+    textAlign: "center",
+    marginVertical: 60,
+  },
+})
